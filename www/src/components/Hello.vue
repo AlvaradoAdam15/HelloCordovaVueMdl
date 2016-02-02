d075958c84277d07b9140daf5fdfb909a175d716
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-02-02 20:56:37 +0100

    Add border style in h1 and lorem

--- www/src/components/Hello.vue
+++ www/src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem beatae blanditiis, enim eveniet id magnam quae quod recusandae sint totam, vel, vitae voluptatem? Corporis cupiditate doloribus nisi veniam vitae.
   </div>
 </template>
 
@@ -21,5 +22,6 @@ export default {
 <style>
   .hello {
     background-color: #00b9d8;
+    border: 1px;
   }
 </style>
